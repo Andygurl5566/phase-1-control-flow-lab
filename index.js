529a4601f36c18ea69ac90ea4729f09f71dfa402
Andrea Freeman <afreeman5566@gmail.com>
2021-09-20 16:13:49 -0500

    commit tree 5587bcef08d33ed48c9cf51217235e1a5bb430bb

--- index.js
+++ index.js
@@ -1,11 +1,44 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(value){
+  // Write your code here!
+  if (value <= 199){
+    return "This one is on me!"
+
+  } //else if (value < 2500 >= 2000 ){
+    //return 'I will gladly take your thirty bucks.'
+
+    else if (value < 2500 & value >= 2000 ){
+      return 'I will gladly take your thirty bucks.'
+
+  } else if (value > 2500) {
+    return "No can do."
+  }
+  
+}
+
+
+
+function ternaryCheckCity(city){
+  if(city === "NYC") {
+    return 'Ok, sounds good.'
+  }
+  else if( city !== "NYC"){
+    return "No go."
+  }
   // Write your code here!
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+function switchOnCharmFromTip(tip){
+  switch (tip) {
+     case "generous":
+      return "Thank you so much." 
+      break
+      case "not as generous":
+      return "Thank you."
+      break
+      default:
+      return "Bye."
+  }
 
-function switchOnCharmFromTip(){
+  //https://www.w3schools.com/js/js_switch.asp
   // Write your code here!
 }
